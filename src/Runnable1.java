7ed77cb6e54e6eaf1b45d6f88354263aba7761b9
wangsz <wangsz@guangeryi.com>
2020-09-13 10:49:24 +0800

    Initial commit

---
+++ src/Runnable1.java
@@ -0,0 +1,27 @@
+/**
+ * Created by @author wangsz on 2020/9/3 22:22
+ *
+ * 实现Runnable接口
+ * 实现接口Runnable具有多线程能力
+ * 启动线程：传入目标对象+Thread对象.start()
+ * 推荐使用：避免单继承局限性，灵活方便，方便同一个对象被多个线程使用
+ */
+public class Runnable1 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("i am looking code ---" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Runnable1 runnable1 = new Runnable1();
+
+        new Thread(runnable1).start();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("i am learning thread ---" + i);
+        }
+    }
+}
---
+++ src/Thread1.java
@@ -0,0 +1,28 @@
+/**
+ * Created by @author wangsz on 2020/9/3 22:11
+ *
+ * 继承Thread类
+ * 子类继承Thread类具备多线程的能力
+ * 启动线程：子类对象.start()
+ * 不建议使用：避免OOP单继承局限性
+ */
+public class Thread1 extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("i am looking code ---" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+
+        //thread1.start();
+        thread1.start();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("i am learning thread ---" + i);
+        }
+    }
+}
