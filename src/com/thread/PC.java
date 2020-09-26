ef94e3b625b7d9c726594a41969b445beb799dc0
wangsz <wangsz@guangeryi.com>
2020-09-26 22:10:45 +0800

    11

---
+++ src/com/thread/PC.java
@@ -0,0 +1,112 @@
+package com.thread;
+
+/**
+ * Created by @author wangsz on 2020/9/26 21:39
+ * 测试：生产者消费者模型---->利用缓冲区解决:管程法
+ */
+//生产者 消费者 产品 缓冲区
+public class PC extends Thread{
+
+    public static void main(String[] args) {
+        SynContainer synContainer = new SynContainer();
+
+        new Productor(synContainer).start();
+        new Consumer(synContainer).start();
+    }
+}
+
+//生产者
+class Productor extends Thread {
+    SynContainer container;
+
+    public Productor(SynContainer container) {
+        this.container = container;
+    }
+
+    //生产
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            container.push(new Chicken(i));
+            System.out.println("生产了" + i + "只鸡");
+        }
+    }
+}
+
+//消费者
+class Consumer extends Thread {
+    SynContainer container;
+
+    public Consumer(SynContainer container) {
+        this.container = container;
+    }
+
+    //消费
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("消费了--->" + container.pop().id + "只鸡");
+        }
+    }
+}
+
+//产品
+class Chicken {
+    int id; //产品编号
+
+    public Chicken(int id) {
+        this.id = id;
+    }
+}
+
+//缓存区
+class SynContainer {
+    //需要一个容器大小
+    Chicken[] chickens = new Chicken[10];
+
+    int count = 0;
+
+    public synchronized void push(Chicken chicken) {
+        //如果容器满了 就需要等待消费者消费
+        while (count == chickens.length) {
+            //通知消费者消费 生产等待
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //如果没有满 我们就要丢入产品
+        chickens[count] = chicken;
+        count++;
+
+        //可以通知消费者消费了
+        this.notifyAll();
+    }
+
+    //消费者消费产品
+    public synchronized Chicken pop() {
+        //判断能否消费
+        while (count == 0) {
+            //等待生产者生产 消费者等待
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //如果可以消费
+        count--;
+        Chicken chicken = chickens[count];
+
+        //吃完了，通知生产者生产
+        this.notifyAll();
+        return chicken;
+    }
+}
+
+
+
+
